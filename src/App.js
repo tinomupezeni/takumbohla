a01c51d2024de2312b549843c4c23736947ded9a
Tino <mupezeni2001@gmail.com>
2023-02-27 13:46:10 -0800

    linking

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route} from 'react-router-dom'
+import {  Route, HashRouter} from 'react-router-dom'
 import Home from "./routes/Home";
 import Contact from './routes/Contact';
 import Ingredients from './routes/Ingredients';
@@ -6,11 +6,11 @@ import Ingredients from './routes/Ingredients';
 function App(){
   return(
     <div className="App">
-      <Routes>
+      <HashRouter>
         <Route path='/' element={<Home/>} />
         <Route path='/contact' element={<Contact/>} />
         <Route path='/ingredients' element={<Ingredients/>} />
-      </Routes>
+      </HashRouter>
     </div>
   )
 }
